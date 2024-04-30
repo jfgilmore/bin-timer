778e2ae9202ead7b17717354739a7e7d30d3c855
Joshua Gilmore <29514079+jfgilmore@users.noreply.github.com>
2024-04-30 23:07:04 +1000

    Changed scheduler to Ticker

--- bin-timer.ino
+++ bin-timer.ino
@@ -1,10 +1,12 @@
 #include <ESP8266WiFi.h>
-#include <NTPClient.h>
 #include <WiFiUdp.h>
+#include <NTPClient.h>
 #include <TimeLib.h>
 #include <RTCVars.h>
-#include <Scheduler.h>
+#include <Ticker.h>
 #include <LeanTask.h>
+
+ADC_MODE(ADC_VCC);
 
 // Define the pins for the LEDs, button, and buzzer
 const uint8_t yellowLEDPin = 5; // D1
@@ -25,9 +27,11 @@ const uint timeUpdateInterval = 1440; // In minutes, 1440 = 1 day
 const uint8_t OFF = LOW;
 const uint8_t ON = HIGH;
 
-// Buzzer Sound duration and interval
+// Buzzer, sound duration and interval
+Ticker ticker;
+bool buzzerOn = false;
 const uint8_t buzzTime = 1; // In seconds
-const uint8_t buzzInterval = 5; // In minutes
+const uint8_t buzzerInterval = 5; // In minutes
 
 // Define persisted state
 RTCVars state; // create the state object
@@ -36,8 +40,8 @@ int recycling;
 
 // Other variables
 uint8_t btnState = 0;
-uint8_t lightsTriggered = 0;
-const uint8_t binDay = 5;
+uint8_t lightsTriggered = false;
+const uint8_t binDay = 2;
 const uint8_t buzzAfter = 20;
 const uint8_t lightsOnAt = 17;
 const time_t initRecyclingDate = 1715040000;
@@ -47,28 +51,16 @@ int currentHours = 0;
 WiFiUDP ntpUDP;
 NTPClient timeClient(ntpUDP, ntpServer, timeZoneOffset * 3600, timeUpdateInterval * 60000);
 
-class BuzzerTask : public LeanTask {
-  protected:
-    void setup() {
-      pinMode(buzzerPin, OUTPUT);
-      noTone(buzzerPin);
-    }
+void blink() {
+  uint8_t state = !digitalRead(redLEDPin);
+  digitalWrite(redLEDPin, state);
+  delay(100);
+  digitalWrite(redLEDPin, !state);
+}
 
-    void loop() {
-      if (!(lightsTriggered == 1 && currentHours >= buzzAfter)) {
-        noTone(buzzerPin);
-      } else {
-        // If it's past 8 PM, start buzzing every 10 minutes until the button is pressed
-        beepBuzzer();
-        delay(buzzInterval * 60 * 1000);
-      }
-    }
-
-    void beepBuzzer() {
-      tone(buzzerPin, 2000, buzzTime * 1000);
-    }
-} buzzer_task;
-
+void buzz() {
+  tone(buzzerPin, 2000, buzzTime * 1000);
+}
 
 void connectWiFi() {
   WiFi.begin(ssid, password);
@@ -81,7 +73,7 @@ void connectWiFi() {
 }
 
 void ICACHE_RAM_ATTR handleButtonPress() {
-  if (btnState != 1 && lightsTriggered == 1) {
+  if (btnState != 1 && lightsTriggered) {
     btnState = 1;
   }
 }
@@ -94,8 +86,46 @@ void buttonPressTasks() {
     digitalWrite(yellowLEDPin, OFF);
     digitalWrite(greenLEDPin, OFF);
 
-    btnState = 0;
     sleepTime = 1;
+    state.saveToRTC();
+    ESP.deepSleep(SECS_PER_HOUR * 1000000, WAKE_RF_DISABLED);
+}
+
+void fullBoot() {
+  connectWiFi();
+
+  // Get time from server
+  timeClient.begin();
+  timeClient.update();
+}
+
+uint16_t voltage() {
+  uint16_t volt = ESP.getVcc();
+  Serial.println("Voltage: " + (String)volt);
+  return volt;
+}
+
+void powerSaving() {
+  if (sleepTime > 1) {
+    Serial.println("Going to sleep... " + (String)sleepTime);
+    ESP.deepSleepInstant(SECS_PER_MIN * 1000000, WAKE_RF_DISABLED);
+  } else if (sleepTime == 1) {
+    Serial.println("Going to sleep, one last time...");
+    ESP.deepSleepInstant(SECS_PER_HOUR * 1000000, WAKE_RFCAL);
+  }
+}
+
+const int tempo = 124;
+const int baseDuration = 60 * 1000 / tempo;
+float duration[] = {1.5, 0.5, 1, 1, 0.5, 2, 1};
+int frequency[] = {220, 220, 247, 220, 196, 175, 165};
+
+void playTune() {
+  for (int i = 0; i < (sizeof(duration)/sizeof(*duration)); i++) {
+    float d = baseDuration * duration[i];
+    tone(buzzerPin, frequency[i], d);
+    delay(d * 1.2);
+  }
 }
 
 void setup() {
@@ -107,6 +137,7 @@ void setup() {
   pinMode(greenLEDPin, OUTPUT);
   pinMode(buttonPin, INPUT);
   pinMode(resetPin, INPUT);
+  pinMode(buzzerPin, OUTPUT);
 
   digitalWrite(redLEDPin, OFF);
   digitalWrite(yellowLEDPin, OFF);
@@ -119,19 +150,22 @@ void setup() {
   state.registerVar(&sleepTime);
   state.registerVar(&recycling);
 
-  if (!state.loadFromRTC()) {
-    // if (sleepTime > 0) {
-    //   sleepTime++;
-    //   state.saveToRTC();
-    // }
-    // Serial.println("This is reset no. " + (String)(sleepTime));
-  // } else {
-    Serial.println("Cold boot, no RTC memory.");
-    connectWiFi();
-    // Get time from server
-    timeClient.begin();
-    timeClient.update();
+  if (state.loadFromRTC()) {
+    sleepTime--;
+    state.saveToRTC();
 
+    // powerSaving();
+    if (voltage() < 3100) {
+      // Low voltage recharge battery
+      playTune();
+      ticker.attach_scheduled(5, blink);
+    }
+
+    fullBoot();
+  } else {
+    Serial.println("Cold boot, no RTC memory.");
+
+    fullBoot();
     // Calculate if recycling or green waste
     const long diff = abs(initRecyclingDate - timeClient.getEpochTime());
     const int recyclingDays = diff / SECS_PER_DAY;
@@ -140,36 +174,21 @@ void setup() {
     state.saveToRTC();
   }
 
-  Scheduler.start(&buzzer_task);
-  Scheduler.begin();
+  // Calculate number of times to sleep before next recycling date after time set
+  // Make sure if woken up out of cycle to reconnect and determine cycles again
+  // Or if time too far away once cycles complete to reinitialise more sleep cycles and enter sleep cycle again
 }
 
 void loop() {
   timeClient.update();
-  yield();
+
   if (btnState == 1) buttonPressTasks();
 
   int dayOfWeek = timeClient.getDay(); // Get the current day of the week (from 0-6 where 0=Sunday)
   currentHours = timeClient.getHours();
 
-  if (dayOfWeek == binDay && currentHours > lightsOnAt) {
-    sleepTime = 0;
-  }
-
-  if (sleepTime == 1) {
-    // const WakeMode mode = currentHours < lightsOnAt - 1 ? WAKE_RF_DISABLED : RF_CAL;
-    Serial.println("Time is: " + timeClient.getFormattedTime());
-    Serial.println("Going to sleep...");
-    state.saveToRTC();
-    ESP.deepSleep(SECS_PER_MIN * 1000000);
-  }
-
-  if (!WiFi.isConnected()) {
-    connectWiFi();
-  }
-
   // Check for bin night and turn on the appropriate LED
-  if (dayOfWeek == binDay && lightsTriggered == 0) {
+  if (dayOfWeek == binDay && !lightsTriggered) {
     digitalWrite(redLEDPin, ON); // Rubbish every Tuesday
     Serial.println(recycling);
     if (recycling == 1) {
@@ -180,7 +199,12 @@ void loop() {
       recycling = 1;
     }
     state.saveToRTC();
-    lightsTriggered = 1;
+    lightsTriggered = true;
+  }
+
+  if (lightsTriggered == 1 && !buzzerOn) {
+    ticker.attach_scheduled(buzzerInterval * 60, buzz);
+    buzzerOn = true;
   }
 }
 
